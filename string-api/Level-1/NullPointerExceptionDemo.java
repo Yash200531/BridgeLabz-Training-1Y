3aaddcc8473ddaeadbdbdab726f067abbef09527
Yash200531 <yashsendit@gmail.com>
2026-02-15 20:48:52 +0530

    Added String API Level 1, Level 2 and Level 3 practice programs

---
+++ string-api/Level-1/NullPointerExceptionDemo.java
@@ -0,0 +1,15 @@
+public class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int length = getNullStringLength();
+            System.out.println("Length: " + length);
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        }
+    }
+
+    private static int getNullStringLength() {
+        String text = null;
+        return text.length();
+    }
+}
