59d430623c431ddb407936fd0efcdcb48ac02736
Yash <67813982+Yash200531@users.noreply.github.com>
2026-02-20 20:05:06 +0530

    Delete methods directory

--- methods/src/com/gla/methods/level1/Main.java
+++
@@ -1,7 +0,0 @@
-package com.gla.methods.level1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starter for methods level1");
-    }
-}
--- methods/src/com/gla/methods/level2/Main.java
+++
@@ -1,7 +0,0 @@
-package com.gla.methods.level2;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starter for methods level2");
-    }
-}
