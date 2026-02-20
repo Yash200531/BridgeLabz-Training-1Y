20eecf06007b5526ae44b30020ba52b67f1b9b6c
Yash200531 <yashsendit@gmail.com>
2026-02-20 21:03:35 +0530

    Updated methods latest files

--- methods/src/com/gla/methods/level1/Main.java
+++ methods/src/com/gla/methods/level1/Main.java
@@ -2,6 +2,8 @@ package com.gla.methods.level1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starter for methods level1");
+        System.out.println("Methods Level 1 ready.");
+        System.out.println("Sample: SumNaturalNumbers.sumUsingFormula(10) = " + SumNaturalNumbers.sumUsingFormula(10));
+        System.out.println("Sample: HandshakeCalculator.calculateHandshakes(5) = " + HandshakeCalculator.calculateHandshakes(5));
     }
 }
--- methods/src/com/gla/methods/level2/Main.java
+++ methods/src/com/gla/methods/level2/Main.java
@@ -2,6 +2,8 @@ package com.gla.methods.level2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starter for methods level2");
+        System.out.println("Methods Level 2 ready.");
+        System.out.println("Sample leap year check (2024): " + LeapYearChecker.isLeapYear(2024));
+        System.out.println("Sample km to miles (10): " + UnitConverterPart1.kilometersToMiles(10));
     }
 }
